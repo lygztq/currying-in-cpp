1e829b530616414550f5adfe8431160a3b805ba5
lygztq <lygztq@gmail.com>
2022-12-21 23:02:10 +0800

    update

--- devtest.cc
+++ devtest.cc
@@ -7,6 +7,9 @@ char foo(int a, float b, char c, bool d) {
 }
 
 int main() {
-  std::cout << make_currying(foo)(1)(0.1)('c')(false) << std::endl;
+  // std::cout << make_currying(foo)(1)(0.1)('c')(false) << std::endl;
+  //auto c = make_currying(foo)(1)(0.1)('c')(false);
+  auto f1 = make_currying(foo)(1);
+  std::cout << f1(0.1)('c')(false) << std::endl;
   return 0;
 }
